40e3aa02191465760673222ae9f70dd200926ecf
millan89 <millan026@hotmail.com>
2020-11-10 22:47:56 +0100

    radio na games carouselu

--- js/main.js
+++ js/main.js
@@ -4,9 +4,36 @@ $(document).ready(function(){
     // SLICK
     if ($('.hero').length) {
 
-        // Initialize hero carousel
+        // Hero carousel
         $('.hero').slick({
             dots: true
+        });
+
+        // Game carousel     
+        $('.games').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 4,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ]
         });
 
     }
